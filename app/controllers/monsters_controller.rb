054ad2a53eec2f412b17860b243c62917fa314c0
Nepsaco <tobie.tsuzuki@gmail.com>
2019-11-05 10:55:54 -0700

    first commit

---
+++ app/controllers/monsters_controller.rb
@@ -0,0 +1,14 @@
+class MonstersController < ApplicationController
+
+  def index
+    monsters = Monster.all
+
+    render json: monsters
+  end
+
+  def show
+    monster = Monster.find(params[:id])
+    render json: monster
+  end
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Monster.destroy_all
+
+Monster.create(img: "https://www.venganza.org/wp-content/uploads/2006/08/FlyingSpaghetti_Dunbar_sm.jpg",quote: "Disclaimer: While Pastafarianism is the only religion based on empirical evidence, it should also be noted that this is a faith-based book. Attentive readers will note numerous holes and contradictions throughout the text; they will even find blatant lies and exaggerations. These have been placed there to test the reader's faith.")
+Monster.create(img: "https://www.venganza.org/wp-content/uploads/2011/05/Octopodes_TheRapture_300.jpg", quote:"Someone has described religious warfare as “killing people over who has the best invisible friend." )
+Monster.create(img: "https://www.venganza.org/images/spreadword/th_havetouched.jpg", quote: "Today Plato is nearly forgotten. His beliefs include the notion that people who govern should be intelligent, rational, self-controlled, and in love with wisdom, an idea that has long been discredited.")
+Monster.create(img: "https://www.venganza.org/images/spreadword/th_obey1.jpg", quote: "We are not saying that Evolution can't exist, only that it is guided by His Noodly Appendage.")
+Monster.create(img: "https://www.venganza.org/images/spreadword/th_pastafish4.jpg", quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+Monster.create(img: , quote: )
+
+
